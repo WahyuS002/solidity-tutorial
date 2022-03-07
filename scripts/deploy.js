70b8046fe0dd76f100e69b4e8e58b911d260627f
WahyuS002 <syahputrawahyu61@gmail.com>
2022-03-07 21:33:27 +0700

    :white_check_mark: #2 State Variables

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,23 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const SimpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
+  const simpleStorage = await SimpleStorage.deploy();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await simpleStorage.deployed();
 
-  await greeter.deployed();
+  console.log("This smart contract is deployed to:", simpleStorage.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+  const text = await simpleStorage.text();
+  console.log(`text is ${text}`);
+
+  const set = await simpleStorage.set("Wahyu Syahputra");
+  console.log(`updated text is ${set}`);
+
+  const get = await simpleStorage.get();
+  console.log(`get text is ${get}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
